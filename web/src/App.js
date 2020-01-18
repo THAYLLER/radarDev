d1ce76e13caf7da052ed140eb7312b66147d6592
Thayller Vilela Cintra <thayllervilelacintra@yahoo.com.br>
2020-01-18 17:31:06 -0300

    versão web
    Implementando layout da versão web e integrando as chamadas da api

---
+++ web/src/App.js
@@ -0,0 +1,55 @@
+import React, {useState,useEffect} from 'react';
+
+import api from './services/api';
+
+import './Global.css';
+import './App.css';
+import './Main.css';
+import './Sidebar.css';
+
+import DevItem from './components/DevItem';
+import DevForm from './components/DevForm';
+
+
+function App() {
+  
+  const [devs, setDevs] = useState([]);
+
+  
+
+  useEffect(() => {
+
+    async function loadDevs() {
+
+      const responde = await api.get('/devs');
+
+      setDevs(responde.data);
+    }
+    loadDevs();
+  },[]);
+
+  async function handleAddDev(data) {
+
+    const response = await api.post('/devs', data)
+
+    setDevs([...devs, response.data]);
+  }
+  return (
+    <div className="App" id="app">
+      <aside>
+        <strong>Cadastrar</strong>
+        <DevForm onSubmit={handleAddDev}/>
+      </aside>
+      <main>
+        <ul>
+          {devs.map(dev =>(
+
+            <DevItem key={dev._id} dev={dev} />
+          ))}
+        </ul>
+      </main>
+    </div>
+  );
+}
+
+export default App;
